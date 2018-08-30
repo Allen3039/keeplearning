8e7cfcdba1db017898834cee3adb97acf50fe2ba
魏开 <hzweikai@corp.netease.com>
2018-08-30 10:09:48 +0800

    tt commit

--- test.js
+++ test.js
@@ -1,43 +1,50 @@
 const issues = {};
 issues.watchers = [];
-issues.listen = (fn) => {
+issues.listen = fn => {
   issues.watchers.push(fn);
-}
+};
 
 issues.trigger = function() {
-  for (let i = 0, watchers = this.watchers, watcher; watcher = watchers[i]; i++) {
+  for (
+    let i = 0, watchers = this.watchers, watcher;
+    (watcher = watchers[i]);
+    i++
+  ) {
     watcher.apply(this, arguments);
   }
-}
+};
 // arguments 不能在箭头函数中使用
-issues.listen(function() { console.log("watcher2", JSON.stringify(arguments)) });
+issues.listen(function() {
+  console.log("watcher2", JSON.stringify(arguments));
+});
 
-issues.listen(() => { console.log("watcher1", JSON.stringify(arguments)) });
-
-
+issues.listen(() => {
+  console.log("watcher1", JSON.stringify(arguments));
+});
 
 issues.trigger("发钱啦", "1个亿呢");
 
-
 // define
-(function (window) {
+(function(window) {
   function fn(str) {
-      this.str = str;
+    this.str = str;
   }
 
-  fn.prototype.format = function () {
-      var arg =Array.from(arguments);
-      return this.str.replace(/\{(\d)\}/g, function (a, b) {
-          console.log(a)
-          return arg[b] || '';
-      });
+  fn.prototype.format = function() {
+    var arg = Array.from(arguments);
+    return this.str.replace(/\{(\d)\}/g, function(a, b) {
+      console.log(a);
+      return arg[b] || "";
+    });
   };
 
   window.fn = fn;
 })(window);
 
 // use
-(function () {
+(function() {
   var t = new fn('<p><a href="{0}">{1}</a><span>{2}</span></p>');
-  console.log(t.format('http://www.alibaba.com', 'Alibaba', 'Welcome'));
+  console.log(t.format("http://www.alibaba.com", "Alibaba", "Welcome"));
 })();
+
+// tt commit
